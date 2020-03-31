053bcc462977e727b72f4bbb24c86f34daa4e58c
Mikhail <mizolotu@jyu.fi>
2020-03-31 17:51:35 +0300

    31.3.2020

--- algorithms/bs_common/models.py
+++ algorithms/bs_common/models.py
@@ -71,9 +71,8 @@ def build_impala_cnn(unscaled_images, depths=[16,32,32], **conv_kwargs):
 
     return out
 
-
 @register("mlp")
-def mlp(num_layers=2, num_hidden=64, activation=tf.tanh, layer_norm=False):
+def mlp(num_layers=2, num_hidden=512, activation=tf.tanh, layer_norm=False):
     """
     Stack of fully-connected layers to be used in a policy / q-function approximator
 
@@ -98,11 +97,8 @@ def mlp(num_layers=2, num_hidden=64, activation=tf.tanh, layer_norm=False):
             if layer_norm:
                 h = tf.contrib.layers.layer_norm(h, center=True, scale=True)
             h = activation(h)
-
         return h
-
     return network_fn
-
 
 @register("cnn")
 def cnn(nh=64):
